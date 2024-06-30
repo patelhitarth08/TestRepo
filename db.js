d2a0a6e7bee57e9c03c5ee78096a7860e05e7bba
Hitarth <patelhitarth08@gmail.com>
2024-06-30 23:55:57 +0530

    changed mongo uri

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/user", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
